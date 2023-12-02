fbebdfde672a73a876c76b7020af114d3223c3e6
rahulfordev <rahulforsms@gmail.com>
2023-12-02 21:49:07 +0600

    user and post details style add

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
+import "./PostDetails.css";
 
 const PostDetails = () => {
   const { userId, id, title, body } = useLoaderData();
   return (
-    <div>
-      <h3>User ID: {userId}</h3>
-      <h3>ID: {id}</h3>
-      <h3>Title: {title}</h3>
-      <h3>Body: {body}</h3>
-      <Link to={`/posts`}>
-        <button>Back to All Posts</button>
-      </Link>
+    <div className="posts__details">
+      <div className="psots__detail--one">
+        <div className="post__details--container">
+          <h3 className="post__details--id">User ID: {userId}</h3>
+          <h3 className="post__details--id">ID: {id}</h3>
+          <h3 className="post__details--title">Title: {title}</h3>
+          <h3 className="post__details__body">Body: {body}</h3>
+        </div>
+        <Link to={`/posts`}>
+          <button>Back to All Posts</button>
+        </Link>
+      </div>
     </div>
   );
 };
